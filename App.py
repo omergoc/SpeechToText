8962854bd3c608a67d66ee742f46eb442a652be8
Ömer Göç <74094920+omergoc@users.noreply.github.com>
2021-12-01 22:52:15 +0300

    Update
    01/12/2021

--- App.py
+++ App.py
@@ -1,20 +1,19 @@
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtCore import  Qt, QUrl, QSize,QCoreApplication
+from PyQt5.QtCore import  Qt, QUrl, QSize,QCoreApplication,pyqtSignal, QThread
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLineEdit,
         QPushButton, QSlider, QStyle, QVBoxLayout, QWidget, QPlainTextEdit, QStatusBar,QProgressBar,QMessageBox)
 from ibm_watson import SpeechToTextV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from datetime import timedelta
-from moviepy.editor import VideoFileClip
+from moviepy.video.io.VideoFileClip import VideoFileClip
 from googletrans import Translator
 from pytube import YouTube 
 import requests
 import os
-import re
 import time
 import json
+
 
 
 class App(QWidget):
@@ -29,7 +28,6 @@ class App(QWidget):
 
         openButton = QPushButton("Video Seç")   
         openButton.setToolTip("Video Seç")
-        # openButton.setStatusTip("Video Seç")
         openButton.setFixedHeight(24)
         openButton.setIconSize(btnSize)
         openButton.setFont(QFont("Noto Sans", 8))
@@ -64,13 +62,13 @@ class App(QWidget):
         self.textArea = QPlainTextEdit(self)
         self.textArea.setMaximumHeight(100)
 
-        self.trConvert = QPushButton("Türkçe Çeviri /// Alt Yazı Dosyası Oluştur")
+        self.trConvert = QPushButton("Türkçe Çeviri ")
         self.trConvert.clicked.connect(self.translateTR)
 
-        self.n = 500
+        self.n = 0
         self.progressBar = QProgressBar()
         self.progressBar.setMinimum(0)
-        self.progressBar.setMaximum(self.n)
+        self.progressBar.setMaximum(400)
         
         layout = QVBoxLayout()
         layout.addWidget(self.youtubeUrl)
@@ -95,22 +93,21 @@ class App(QWidget):
             QMessageBox.about(self, "Hata", "İnternet Bağlantınız Bulunmamaktadır.")
             QCoreApplication.quit()
 
+
     def NetworkControl(self):
         r = requests.head("https://www.turkhackteam.org/")
         return r.status_code == 200
+
 
     def youtubeDownload(self):
         controlVideo= self.youtubeUrl.text()
         if controlVideo == "":
             self.statusBar.showMessage("Adres Alanı Boş Olamaz")
         else:
-            self.statusBar.showMessage("Video İndiriliyor")
-            self.run()
-            time.sleep(1)
-            video_url = YouTube(controlVideo)
-            videos = video_url.streams.filter(file_extension='mp4').first()
-            videos.download()
-            self.statusBar.showMessage("İşlem Tamamlandı")
+            self.statusBar.showMessage("Video İndiriliyor.")
+            self.download = threadDownload(self.youtubeUrl.text())
+            self.download.sonuc.connect(self.Finish)
+            self.download.start()
 
 
     def openFile(self):
@@ -122,117 +119,71 @@ class App(QWidget):
                     QMediaContent(QUrl.fromLocalFile(fileName)))
             self.playButton.setEnabled(True)
             self.statusBar.showMessage(fileName)
+
             try:
                 self.videoConvert(fileName)
             except:
-                QMessageBox.about(self, "Hata", "Video Dosyası Bulunamadı.")
+                QMessageBox.about(self, "Hatalı Video", "MP4 Dosyası Bulunamadı Yada Bozuk.")
                 QCoreApplication.quit()
-            try:
-                self.jsonCreate()
-            except:
-                QMessageBox.about(self, "Hatalı API", "API.TXT Dosyası Bulunamadı Yada Hatalı.")
-                QCoreApplication.quit()
-            try:
-                self.jsonRead()
-            except:
-                QMessageBox.about(self, "Hata", "data.json Dosyası Bulunamadı.")
-                QCoreApplication.quit()
-            try:
-                self.WriteText()
-            except:
-                QMessageBox.about(self, "Hata", "Veri.Txt Dosyası Bulunamadı.")
-                QCoreApplication.quit()
+
             self.play()
 
 
     def videoConvert(self,video):
-        video = VideoFileClip(os.path.join(video))
-        self.statusBar.showMessage("Ses Ayıklama İşlemi Yapılıyor")
-        time.sleep(1)
-        self.run()
-        video.audio.write_audiofile(os.path.join("ses.mp3"))
+        self.statusBar.showMessage("Ses Ayıklama İşlemi Başlatıldı.")
+        self.ConvertMp3 = threadVideoConvert(video)
+        self.ConvertMp3.result.connect(self.jsonCreate)
+        self.ConvertMp3.start()
+        self.ProgressBar()
 
 
     def jsonCreate(self):
-        self.statusBar.showMessage("Json Dosyası Oluşturuluyor")
-        time.sleep(1)
-        self.run()
-        with open("API.txt", "r") as f:
-            api_txt = f.read()
-
-        api=IAMAuthenticator(api_txt)
-        speech_2_text = SpeechToTextV1(authenticator=api)
-        speech_2_text.set_service_url("https://api.au-syd.speech-to-text.watson.cloud.ibm.com/instances/2b854a53-1b61-4264-bb6c-0ff850bca6f0")
-
-        with open("ses.mp3","rb") as audio_file:
-            global result
-            result= speech_2_text.recognize(
-                audio=audio_file,content_type="audio/mp3"
-            ).get_result()
-
-        with open('data.json', 'w') as f:
-            json.dump(result, f)
+        self.statusBar.showMessage("Json Dosyası Oluşturuluyor.")
+        self.readJson = threadJsonCreate()
+        self.readJson.result.connect(self.jsonRead)
+        self.readJson.start()
+        self.ProgressBar()
 
 
     def jsonRead(self):
-        time.sleep(1)
-        self.statusBar.showMessage("Json Dosyası Okunuyor")
-        self.run()
-        with open('data.json', 'r') as openfile:
-            self.x = json.load(openfile)
-            array = []
-            y = self.x['results']
-
-            for item in y:
-                for item in item['alternatives']:
-                    array.append(item['transcript'])
-
-            text = str(array).replace('"',"").replace("[","").replace("]","")
-
-            with open("Veri.txt", "w") as f:
-                f.write(text)
+        self.statusBar.showMessage("Json Dosyası Okunuyor.")
+        self.readJson = threadJsonRead()
+        self.readJson.result.connect(self.WriteText)
+        self.readJson.start()
+        self.ProgressBar()
 
 
     def WriteText(self):
-        time.sleep(1)
-        self.statusBar.showMessage("Video Başlatıldı")
-        with open("Veri.txt", "r") as file:
+        with open("Veri.txt", "r", encoding="utf8") as file:
             text = file.read()
             self.textArea.clear()
             self.textArea.insertPlainText(text)
+        self.statusBar.showMessage("İşlem Tamamlandı.")
+        self.ProgressBar()
 
 
     def translateTR(self):
         try:
-            time.sleep(1)
-            self.statusBar.showMessage("Türkçe Diline Çeviriliyor")
-            self.run()
+            self.statusBar.showMessage("Türkçe Diline Çeviriliyor.")
             self.textArea.clear()
-
-            with open("Veri.txt", "r") as file:
+            with open("Veri.txt", "r", encoding="utf8") as file:
                 global text
                 text = file.read()
-
-            translator = Translator()
-            example = translator.translate(text,dest="tr")
-            with open("Veri.txt", "w") as f:
-                f.write("<font face='Cronos Pro Light' size='8' color='#ff0000'>")
-                f.write(example.text)
-                f.write("</font>")
-
-            self.TxtToSrt()
-            self.textArea.insertPlainText(example.text)
-            self.statusBar.showMessage("İşlem Tamamlandı")
-            self.progressBar.setValue(0)
+            self.createTranslateTR = threadTranslateTR(text)
+            self.createTranslateTR.result.connect(self.ProgressBar)
+            self.createTranslateTR.start()
+            self.WriteText()
         except:
             QMessageBox.about(self, "Hata", "Veri.Txt Dosyası Bulunamadı.")
             QCoreApplication.quit()
 
 
-    def run(self):
-        for i in range(self.n):
+
+    def translateProgress(self):
+        for i in range(400):
             time.sleep(0.01)
-            self.progressBar.setValue(i+1)
+            self.n  = self.n + 1
+            self.progressBar.setValue(self.n)
 
 
     def play(self):
@@ -268,48 +219,159 @@ class App(QWidget):
         self.statusBar.showMessage("Hata: " + self.mediaPlayer.errorString())
 
 
-    def createSrt(self):
-        with open("altyazi.txt", "w") as f:
-            f.write("1")
-            f.write("00:00:00,000 --> 0:16:40,000")
-            f.write("<font face='Cronos Pro Light' size='8' color='#ff0000'>")
-            f.write("</font>")
+    def ProgressBar(self):
+        for i in range(100):
+            time.sleep(0.01)
+            self.n  = self.n + 1
+            self.progressBar.setValue(self.n)
 
 
-    def TxtToSrt(self):
-        dursec = 1000
+class threadDownload(QThread):
+    result = pyqtSignal(object)
 
-        inputtxt = 'Veri.txt'
-        subpath = os.path.join(os.path.dirname(__file__), inputtxt)
-        subtxt = open(subpath).read()
-
-        par = re.split('\n{2,}', subtxt)
-
-        npar = len(par)
-
-        tdstart = timedelta(hours=0, seconds=-dursec)
-        tddur = timedelta(seconds=dursec)
-
-        tdlist = []
-        for i in range(npar+1):
-            tdstart = tdstart + tddur
-            tdlist.append(tdstart)
-
-        lcomb = []
-        for i in range(npar):
-            lcomb.append(str(i+1) + '\n' + str(tdlist[i]) + ',000 --> ' + str(
-                tdlist[i+1]) + ',000' + '\n' + par[i] + '\n')
+    def __init__(self,link):
+        super().__init__()
+        self.link = link
 
 
-        srtstring = '\n'.join(lcomb)
+    def run(self):
+        try:
+            youtubeDownload = Youtube(self.link)
+            youtubeDownload.Download()
+            self.result.emit("İşlem Tamamlandı.")
+        except:
+            self.result.emit("Beklenmedik Bir Hata Oluştu.")
 
-        pat = r'^(\d:)'
-        repl = '0\\1'
-        srtstring2 = re.sub(pat, repl, srtstring, 0, re.MULTILINE)
 
-        srtout = os.path.join(os.path.dirname(__file__), 'altyazi.srt')
-        with open(srtout, 'w',encoding="utf-8") as newfile:
-            newfile.write(srtstring2)
+class Youtube():
+    def __init__(self,link):
+        self.link = link
+
+    def Download(self):
+        controlVideo= self.link
+        video_url = YouTube(controlVideo)
+        videos = video_url.streams.filter(file_extension='mp4').first()
+        videos.download()
+
+
+class threadVideoConvert(QThread):
+    result = pyqtSignal(object)
+
+    def __init__(self,video):
+        super().__init__()
+        self.video = video
+
+    def run(self):
+        try:
+            videoConvert = VideoConvert(self.video)
+            videoConvert.Mp3Convert()
+            self.result.emit("İşlem Tamamlandı.")
+        except:
+            self.result.emit("Beklenmedik Bir Hata Oluştu.")
+
+
+class VideoConvert():
+    
+    def __init__(self,video):
+        self.video = video
+    
+    def Mp3Convert(self):
+        video = VideoFileClip(os.path.join(self.video))
+        video.audio.write_audiofile(os.path.join("ses.mp3"))
+
+
+class threadJsonCreate(QThread):
+    result = pyqtSignal(object)
+
+    def __init__(self):
+        super().__init__()
+    
+    def run(self):
+        jsonCreate = DataJson()
+        jsonCreate.Create()
+        self.result.emit("İşlem Tamamlandı.")
+
+class threadJsonRead(QThread):
+    result = pyqtSignal(object)
+    
+    def __init__(self):
+        super().__init__()
+    
+    def run(self):
+        try:
+            jsonRead = DataJson()
+            jsonRead.Read()
+            self.result.emit("İşlem Tamamlandı.")
+        except:
+            self.result.emit("Beklenmedik Bir Hata Oluştu.")
+
+class DataJson():
+
+    @staticmethod
+    def Create():
+        with open("API.txt", "r") as f:
+            api_txt = f.read()
+
+        api=IAMAuthenticator(api_txt)
+        speech_2_text = SpeechToTextV1(authenticator=api)
+        speech_2_text.set_service_url("https://api.au-syd.speech-to-text.watson.cloud.ibm.com/instances/2b854a53-1b61-4264-bb6c-0ff850bca6f0")
+
+        with open("ses.mp3","rb") as audio_file:
+            global result
+            result= speech_2_text.recognize(
+                audio=audio_file,content_type="audio/mp3"
+            ).get_result()
+
+        with open('data.json', 'w') as f:
+            json.dump(result, f)
+    
+    @staticmethod
+    def Read():
+        with open('data.json', 'r') as openfile:
+            x = json.load(openfile)
+            array = []
+            y = x['results']
+
+            for item in y:
+                for item in item['alternatives']:
+                    array.append(item['transcript'])
+
+            text = str(array).replace('"',"").replace("[","").replace("]","")
+
+            with open("Veri.txt", "w") as f:
+                f.write(text)
+
+
+class threadTranslateTR(QThread):
+    result = pyqtSignal(object)
+
+    def __init__(self,text):
+        super().__init__()
+        self.text = text
+
+
+    def run(self):
+        try:
+            translateConvert = Translate(self.text)
+            translateConvert.Tr()
+            self.result.emit("İşlem Tamamlandı.")
+        except:
+            self.result.emit("Beklenmedik Bir Hata Oluştu.")
+
+
+class Translate():
+
+    def __init__(self,text):
+        self.text = text
+    
+    def Tr(self):
+
+        translator = Translator()
+        example = translator.translate(self.text,dest="tr")
+        
+        with open("Veri.txt","w", encoding="utf8") as file:
+            file.write(example.text)
+
 
 
 if __name__ == '__main__':
